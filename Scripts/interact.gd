29e3a29b28ce25a4e119e4d196aec665e2f53669
turkeytray <turkeytray07@gmail.com>
2024-10-13 11:09:36 -0600

    Got interactions system set up

---
+++ Scripts/interact.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var inRadius := false
+
+func _ready() -> void:
+	pass 
+
+func _process(_delta: float) -> void:
+	if Input.is_key_pressed(KEY_E) and inRadius:
+		print("Hello")
+
+func _on_player_enter(body: Node2D) -> void:
+	if body.name != "Player":
+		return
+	get_parent().get_parent().get_node("InteractionPopup").visible = true
+	inRadius = true
+
+func _on_player_exited(body: Node2D) -> void:
+	if body.name != "Player":
+		return
+	get_parent().get_parent().get_node("InteractionPopup").visible = false
+	inRadius = false
