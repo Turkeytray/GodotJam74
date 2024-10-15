ef9f0eb68f0d08639ad9fc5abb75a8ee71a90a07
turkeytray <turkeytray07@gmail.com>
2024-10-15 08:55:18 -0600

    got basic lift stuff done

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,26 @@
 extends CharacterBody2D
 
-var speed = 300
+var speed := 300
+var collectedPieces := 5
+
+signal returnPieces
 
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
-	#$Camera/CanvasLayer/ColorRect.get_material().set_shader_parameter("offset", sanity)
+func _process(_delta: float) -> void:
+	if Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down"):
+		$PlayerSprite.play()
+	else:
+		$PlayerSprite.stop()
+	
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = speed * direction
 	
 	move_and_slide()
+	
+func _on_lift_place():
+	if collectedPieces < 0:
+		return
+	returnPieces.emit(collectedPieces)
+	collectedPieces = 0
